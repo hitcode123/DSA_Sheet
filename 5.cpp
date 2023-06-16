1d865dc867fa8ad671e0919269aa7d93a645db6f
hitcode123 <m7870899267@gmail.com>
2023-06-16 13:46:20 +0530

    5th commit

---
+++ 5.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int maximumProfit(vector<int> &prices){
+    int n=prices.size();
+    int maxi=INT_MIN;
+    int result=INT_MIN;
+    for(int i=n-2;i>=0;i--)
+    {
+       maxi=max(prices[i+1],maxi);
+       if(maxi-prices[i]<0)
+       {
+           continue;
+       }
+       else
+       {
+           result=max(result,maxi-prices[i]);
+       }
+
+    }
+   return  result==INT_MIN?0:result;
+    // Write your code here.
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        vector<int> prices(n);
+        for(int i=0;i<n;i++)
+        {
+            int val;
+            cin>>val;
+            prices.push_back(val);
+        }
+        cout<<maximumProfit(prices)<<endl;
+
+
+    }
+}
